6398b587b8be83664404c2a8d9662a3249ae9c5a
JOAO PAULO PIRES DANTAS <econjpaulopd@gmail.com>
2019-04-15 21:50:29 -0300

    Add files via upload

---
+++ Aula1.R
@@ -0,0 +1,16 @@
+attach(aulas)
+section_id
+options(max.print = 40000)
+head(section_id)
+sort(section_id)
+aulas[33137,3]<-3255
+sort(aulas$section_id)
+unique(aulas$section_id)
+length(unique(aulas$section_id))
+table(aulas$section_id)
+sort(table(aulas$section_id))
+sort(table(aulas$course_id))
+install.packages("plyr")
+library(plyr)
+auxiliar<-count(aulas,vars = "course_id")
+write.csv(auxiliar,"popularidade.csv")
